27bbd5e5b033e0b77ed07db362680a1344e1117e
manav mangukiya <55248368+manav88@users.noreply.github.com>
2025-06-21 08:35:15 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -6,16 +6,16 @@ os.makedirs('data3', exist_ok=True)
 
 data_folder = "data/"
 
-# Use glob to find all Excel files in the folder
+
 file_list = glob.glob(data_folder + "*.xls") 
 
 all_frames = []
 
-# Loop through each file path and read it into a DataFrame
+
 for file in file_list:
     print(file)
     if file=="data/indname.xls":
-        #print("h")
+        
         continue
     else:
         df = pd.read_excel(file, skiprows=7,sheet_name='Industry Averages')
@@ -24,16 +24,13 @@ for file in file_list:
             print("here")
             df.columns=df.loc[0]
             df=df.drop(0)
-        filename = os.path.basename(file)  # Extract the file name
+        filename = os.path.basename(file)  
         new_file_path = os.path.join('data2/', filename)  # Create the new file path
         df.to_excel(new_file_path, index=False,engine='openpyxl')
         print("files saved successfully")
         all_frames.append(df)
     
-# merged_df = pd.merge(all_frames,on="Industry Name" ,how="outer")
 
-# Optionally, specify a sheet name to read from each file
-# merged_df = pd.concat([pd.read_excel(file, sheet_name="Sheet1") for file in file_list], ignore_index=True)
 
 # Save the merged DataFrame to a new Excel file
 # merged_df.to_excel("merged_data.xlsx", index=False)
